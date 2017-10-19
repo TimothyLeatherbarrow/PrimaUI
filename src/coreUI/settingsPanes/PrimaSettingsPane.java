5996682dc1463a7e277af24c3cfaa5b247f5ed96
Timothy Leatherbarrow <timothy.leatherbarrow@gmail.com>
2017-10-19 20:00:49 +0100

    Incremental Update

--- src/coreUI/settingsPanes/PrimaSettingsPane.java
+++ src/coreUI/settingsPanes/PrimaSettingsPane.java
@@ -1,13 +1,35 @@
 package coreUI.settingsPanes;
 
 import com.tiggerbiggo.prima.core.Builder;
+import com.tiggerbiggo.prima.presets.TransformTypes;
+import javafx.beans.InvalidationListener;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ChoiceBox;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class PrimaSettingsPane extends SettingsPane{
+
+    Builder b;
+
+    ChoiceBox<TransformTypes> types;
+
 
     public PrimaSettingsPane(Builder b)
     {
         super();
-        if(b == null) 
+        if(b == null) b = new Builder();
+
+        this.b = b;
+    }
+
+    void updateFields()
+    {
+        types.setValue(b.getTransformType());
     }
 
 }
