196332fd7ddbaf0d12df5c1e4bbf7c2aa948534c
Timothy Leatherbarrow <timothy.leatherbarrow@gmail.com>
2017-10-19 16:01:41 +0100

    Added an example FXML file to show possible settings layout

--- src/coreUI/settingsPanes/PrimaSettingsPane.java
+++ src/coreUI/settingsPanes/PrimaSettingsPane.java
@@ -1,5 +1,13 @@
 package coreUI.settingsPanes;
 
+import com.tiggerbiggo.prima.core.Builder;
+
 public class PrimaSettingsPane extends SettingsPane{
+
+    public PrimaSettingsPane(Builder b)
+    {
+        super();
+        if(b == null) 
+    }
 
 }
